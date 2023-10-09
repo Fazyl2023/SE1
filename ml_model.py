f29334cff788e0d29327928ed117a44fcafebf51
Fazyl2023 <146023432+Fazyl2023@users.noreply.github.com>
2023-10-09 13:11:31 +0300

    Add files via upload

---
+++ ml_model.py
@@ -0,0 +1,25 @@
+import uvicorn
+from fastapi import FastAPI
+from model import IrisModel, IrisSpecies
+
+# 2. Create the app object
+app = FastAPI()
+model = IrisModel()
+
+
+@app.post('/predict')
+def predict_species(iris: IrisSpecies):
+    data = iris.dict()
+    prediction, probability = model.predict_species(
+        data['sepal_length'], data['sepal_width'], data['petal_length'], data['petal_width']
+    )
+    return {
+        'prediction': prediction,
+        'probability': probability
+    }
+
+
+# 5. Run the API with uvicorn
+#Отображать в реальном времени
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000) 
